1b2523bf68666d8a112eee8e55c72851243b70c2
dhruba2807 <dhrubomondal1000@gmail.com>
2024-07-24 01:15:38 +0530

    commit done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App(){
+  return(
+    <>
+    <div className="h-screen bg-black flex items-center justify-center" >
+
+    <div  className=" text-5xl text-center   text-red-600  " > Hello World</div>
+    
+   </div> <div>
+  
+    </div>
+    </>
+  )
+}
+export default App
